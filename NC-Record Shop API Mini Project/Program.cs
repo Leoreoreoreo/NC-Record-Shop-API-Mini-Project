02bc56d0ccc59fbea747090e7a532ddb8bddfe9f
Leo Z <jinluezhang@gmail.com>
2026-05-06 13:57:05 +0100

    add: Configure DbContext and repository services in Program.cs

--- NC-Record Shop API Mini Project/Program.cs
+++ NC-Record Shop API Mini Project/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using NC_Record_Shop_API_Mini_Project.Data;
+using NC_Record_Shop_API_Mini_Project.Repositories;
+using NC_Record_Shop_API_Mini_Project.Services;
+
 namespace NC_Record_Shop_API_Mini_Project
 {
     public class Program
@@ -10,6 +15,11 @@ namespace NC_Record_Shop_API_Mini_Project
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                                    options.UseInMemoryDatabase(
+            builder.Configuration.GetConnectionString("DefaultConnection")!));
+            builder.Services.AddScoped<IAlbumRepository, AlbumRepository>();
+            builder.Services.AddScoped<IAlbumService, AlbumService>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
