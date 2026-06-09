11fb17b03bfb94e2dcb68a7d003c8ef2c7fcb40a
Leo Z <jinluezhang@gmail.com>
2026-06-09 11:59:29 +0100

    Move seed data into the DbContext

--- NC-Record Shop API Mini Project/Data/RecordShopDbContext.cs
+++ NC-Record Shop API Mini Project/Data/RecordShopDbContext.cs
@@ -6,8 +6,21 @@
     {
         public  RecordShopDbContext(DbContextOptions<RecordShopDbContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Album> Albums {get; set;}
+
+        public void Seed()
+        {
+            if (Albums.Any()) return;
+            Albums.AddRange(
+                new Album { Name = "Thriller", Artist = "Michael Jackson", Genre = "Pop", ReleaseYear = 1982, Stock = 10, Price = 14.99m },
+                new Album { Name = "Back in Black", Artist = "AC/DC", Genre = "Rock", ReleaseYear = 1980, Stock = 5, Price = 12.99m },
+                new Album { Name = "The Dark Side of the Moon", Artist = "Pink Floyd", Genre = "Progressive Rock", ReleaseYear = 1973, Stock = 8, Price = 16.99m },
+                new Album { Name = "Rumours", Artist = "Fleetwood Mac", Genre = "Soft Rock", ReleaseYear = 1977, Stock = 6, Price = 13.99m },
+                new Album { Name = "Kind of Blue", Artist = "Miles Davis", Genre = "Jazz", ReleaseYear = 1959, Stock = 4, Price = 11.99m }
+            );
+            SaveChanges();
+        }
     }
 }
--- NC-Record Shop API Mini Project/Program.cs
+++ NC-Record Shop API Mini Project/Program.cs
@@ -56,17 +56,7 @@ namespace NC_Record_Shop_API_Mini_Project
             {
                 using var scope = app.Services.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<RecordShopDbContext>();
-                if (!db.Albums.Any())
-                {
-                    db.Albums.AddRange(
-                        new Album { Name = "Thriller", Artist = "Michael Jackson", Genre = "Pop", ReleaseYear = 1982, Stock = 10, Price = 14.99m },
-                        new Album { Name = "Back in Black", Artist = "AC/DC", Genre = "Rock", ReleaseYear = 1980, Stock = 5, Price = 12.99m },
-                        new Album { Name = "The Dark Side of the Moon", Artist = "Pink Floyd", Genre = "Progressive Rock", ReleaseYear = 1973, Stock = 8, Price = 16.99m },
-                        new Album { Name = "Rumours", Artist = "Fleetwood Mac", Genre = "Soft Rock", ReleaseYear = 1977, Stock = 6, Price = 13.99m },
-                        new Album { Name = "Kind of Blue", Artist = "Miles Davis", Genre = "Jazz", ReleaseYear = 1959, Stock = 4, Price = 11.99m }
-                    );
-                    db.SaveChanges();
-                }
+                db.Seed();
             }
 
             app.Run();
