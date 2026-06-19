52e4cdbad9b9e9efd259511cdd360d8258d88df6
Leoreoreoreo <jinluezhang@gmail.com>
2026-06-19 12:45:20 +0100

    fix: Add migration for the Album Price column

---
+++ NC-Record Shop API Mini Project/Migrations/20260619114302_AddPriceToAlbum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NC_Record_Shop_API_Mini_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToAlbum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Albums",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Albums");
+        }
+    }
+}
